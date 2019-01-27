0e893bae27d5116c4e5ad088f314adc26f05f294
huangwch <upyouth@outlook.com>
2019-01-27 10:36:55 +0800

    "test after modify username"

--- microservicecloud-provider-dept-8001/src/main/java/com/hwc/springcloud/DeptProvider8001_APP.java
+++ microservicecloud-provider-dept-8001/src/main/java/com/hwc/springcloud/DeptProvider8001_APP.java
@@ -3,6 +3,9 @@ package com.hwc.springcloud;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 启动部门为服务提供者
+ */
 @SpringBootApplication
 public class DeptProvider8001_APP {
     public static void main(String[] args) {
