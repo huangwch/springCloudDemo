2c1df1e53bfa44cb8c85b146fed5e107c9ec381e
huangwch <upyouth@outlook.com>
2019-01-27 22:28:52 +0800

    增加eureka模块”

--- microservicecloud-provider-dept-8001/src/main/java/com/hwc/springcloud/DeptProvider8001_App.java
+++ microservicecloud-provider-dept-8001/src/main/java/com/hwc/springcloud/DeptProvider8001_App.java
@@ -2,11 +2,15 @@ package com.hwc.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
- * 启动部门为服务提供者
+ * 启动部门微服务提供者
  */
 @SpringBootApplication
+@EnableEurekaClient
+@EnableDiscoveryClient
 public class DeptProvider8001_App {
     public static void main(String[] args) {
         SpringApplication.run(DeptProvider8001_App.class, args);
