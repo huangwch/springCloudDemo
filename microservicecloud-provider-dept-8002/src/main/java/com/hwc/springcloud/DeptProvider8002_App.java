4999353ac7b0b19d1b26b9d86050825e1f6cc6ff
huangwch <upyouth@outlook.com>
2019-01-29 22:17:10 +0800

    增加微服务提供方8002

---
+++ microservicecloud-provider-dept-8002/src/main/java/com/hwc/springcloud/DeptProvider8002_App.java
@@ -0,0 +1,18 @@
+package com.hwc.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * 启动部门微服务提供者
+ */
+@SpringBootApplication
+@EnableEurekaClient
+@EnableDiscoveryClient
+public class DeptProvider8002_App {
+    public static void main(String[] args) {
+        SpringApplication.run(DeptProvider8002_App.class, args);
+    }
+}
---
+++ microservicecloud-provider-dept-8002/src/main/java/com/hwc/springcloud/service/DeptService.java
@@ -0,0 +1,13 @@
+package com.hwc.springcloud.service;
+
+import com.hwc.springcloud.entities.Dept;
+
+import java.util.List;
+
+public interface DeptService {
+    public boolean add(Dept dept);
+
+    public Dept get(Long id);
+
+    public List<Dept> list();
+}
